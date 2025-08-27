53284548397942b48554816a27d94cd04248643b
marvinwankersteen <marvinwankersteen>
2025-08-27 17:39:42 +0200

    Fixed the process of fetching the tokens. It's necessary to set the right user-agent.

--- hyundai_kia_connect_api/KiaFetchApiTokens.py
+++ hyundai_kia_connect_api/KiaFetchApiTokens.py
@@ -116,10 +116,9 @@ def _get_tokens(code):
 def main():
     if len(sys.argv) == 1:
         url = "https://idpconnect-eu.kia.com/auth/api/v2/user/oauth2/authorize?ui_locales=de&scope=openid+profile+email+phone&response_type=code&client_id=peukiaidm-online-sales&redirect_uri=https%3A%2F%2Fwww.kia.com%2Fapi%2Fbin%2Foneid%2Flogin&state=aHR0cHM6Ly93d3cua2lhLmNvbS9kZS8"
-        useragent = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19_CCS_APP_AOS"
 
         print(f"Step 1: Open your Browser (best is Chrome), CTRL+SHIFT+I, CTRL-SHIFT+P, type 'network conditions', uncheck 'Use browser default' and set the following user-agent:\n")
-        print(f"        User-Agent: {useragent}\n")
+        print(f"        User-Agent: {user_agent}\n")
         print(f"Step 2: Open this URL:\n")
         print(f"        {url}\n")
         print(f"Step 3: Solve the reCAPTCHA and login with your credentials. After successful login, you get redirected to Kia's homepage.")
@@ -149,7 +148,7 @@ def main():
         if tokens is not None:
             refresh_token = tokens["refresh_token"]
             access_token = tokens["access_token"]
-            print(f"\n✅ Your tokens are:\n- Refresh Token: {refresh_token}\n- Access Token: {access_token}")
+            print(f"\n✅ Your tokens are:\n\n- Refresh Token: {refresh_token}\n- Access Token: {access_token}")
 
 if __name__ == "__main__":
     main()
